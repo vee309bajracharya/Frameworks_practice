fde47a8dc7e5c32e38a8c89887efb2645fa69c76
vee309bajracharya <verrinshakya309@gmail.com>
2024-08-29 17:58:55 +0545

    tailwind installation

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      colors:{
+        primary: '#3490DC',
+        secondary: '#FFC107',
+        success: '#4CAF50',
+        warning: '#FF9800',
+        danger: '#F44336',
+        info: '#2196F3',
+        light: '#F5F5F5',
+        dark: '#455A64',
+
+      },
+
+      fontFamily:{
+        'orbitron': ['Orbitron', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
